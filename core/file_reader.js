a182359c4ea0b694a919a9300daf34d5fa1f92f4
chocolatada21 <ulisesjustosaucedo@gmail.com>
2026-01-20 11:45:13 -0300

    Refactor file_reader.js

--- core/file_reader.js
+++ core/file_reader.js
@@ -1,7 +1,44 @@
 const fs = require('node:fs');
 
-const apply_variables = (html, variables) => {
+const serch_variable_value = (html, data, index, lastIndex) => {
+    const fullKey = html.substring(index+2, lastIndex);
+    const keyParts = fullKey.split('.');
+    let dataCopy = data;
+    for(let i = 0 ; i < keyParts.length; i++) {
+        if(i === 0) {
+            continue;
+        }
+        dataCopy = dataCopy[keyParts[i]];
+    }
+    
+    return { fullKey, dataCopy };
+}
 
+const search_variable_last_index = (html, index) => {
+    let lastIndex = index;
+    while(lastIndex < html.length || index !== -1) {
+        let char = html.charAt(lastIndex);
+        if(char === '}') {
+            break;
+        }
+        lastIndex += 1;
+    }
+    return lastIndex;
+}
+
+const apply_variables = (html, data, key) => {
+    const regex = new RegExp(`\\$\\{${key}(\\.\\w+)*\\}`);
+    let index = html.search(regex);
+    while(index !== -1) {
+        const lastIndex = search_variable_last_index(html, index);
+
+        const { fullKey, dataCopy } = serch_variable_value(html, data, index, lastIndex);
+
+        html = html.replaceAll(`\${${fullKey}}`, dataCopy);
+
+        index = html.search(regex);
+    }
+    return html;
 }
 
 const load_html = (response, file_path, variables) => {
@@ -9,36 +46,8 @@ const load_html = (response, file_path, variables) => {
 
     if(variables) {
         variables.forEach((variable) => {
-            const key = variable.key;
-            const value = variable.value;
-            const data = JSON.parse(value);
-            
-            const regex = new RegExp(`\\$\\{${key}(\\.\\w+)*\\}`);
-            let index = html.search(regex);
-            while(index !== -1) {
-                let lastIndex = index;
-                while(lastIndex < html.length || index !== -1) {
-                    let char = html.charAt(lastIndex);
-                    if(char === '}') {
-                        break;
-                    }
-                    lastIndex += 1;
-                }
-
-                const fullKey = html.substring(index+2, lastIndex);
-                const keyParts = fullKey.split('.');
-                let dataCopy = data;
-                for(let i = 0 ; i < keyParts.length; i++) {
-                    if(i === 0) {
-                        continue;
-                    }
-                    dataCopy = dataCopy[keyParts[i]];
-                }
-
-                html = html.replaceAll(`\${${fullKey}}`, dataCopy);
-
-                index = html.search(regex);
-            }
+            const data = JSON.parse(variable.value);
+            html = apply_variables(html, data, variable.key);
         });
     }
 
